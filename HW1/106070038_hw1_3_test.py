5d5973b540ae355e0525249301e89f16fc521f6f
wwweiwei <44032506+wwweiwei@users.noreply.github.com>
2021-03-27 16:23:00 +0800

    HW1-3 version 3

--- HW1/106070038_hw1_3_test.py
+++ HW1/106070038_hw1_3_test.py
@@ -1,21 +1,14 @@
-
-import tkinter as tk
-import random 
-import time
 import numpy as np
 import _pickle as cPickle
-from tqdm import tqdm
+import random 
 
 class Agent:
-    """  Class that defines agent and its possible actions """
-
     def __init__(self, symbol, state, load_trainer = None):
         """
         symbol (string)
         state (Board)
         load_trainer (string) - Path to saved trainer
         """
-
         self.symbol = symbol
         self.current_state = state
 
@@ -37,48 +30,8 @@ class Agent:
         """ Update possible actions """
         self.actions = self.current_state.getAvailablePos()
     
-    def performAction(self, action, state = None, updateQ = False):
-        """ 
-            Make move from agent, updates the state and possible actions.
-            Also updates Q at the same time.                        
-        """
-        if action.shape != (2,):
-            print("Wrong shape " + str(action))
-
-        if state == None:
-            state = self.current_state
-
-        # Read action
-        x = action[0]
-        y = action[1]
-        # print("x"+str(x))
-        # print("y"+str(y))
-
-        # Update Q as part of Q-learning in the Trainer class
-        if updateQ is True:
-            self.trainer.updateQ(state, action)
-
-        # Make move
-        state.setPosition(x, y, self.symbol)
-        self.action_history.append(action)
-
-        # Update possible actions
-        self.updatePossibleActions()
-    
-    def performRandomAction(self, updateQ=True):
-        """ Perform random actions, important for exploration of state-pairs """
-        
-        self.updatePossibleActions()
-        random_idx = np.random.choice(self.actions.shape[0])
-        action = self.actions[random_idx]
-
-        self.performAction(action, updateQ=updateQ)
-
-        return action
-
     def revertLastAction(self, state = None):
         """ Make move from agent, updates the state and possible actions  """
-
         if state == None:
             state = self.current_state
 
@@ -100,7 +53,6 @@ class Agent:
 
     def getActionHashFromState(self, action = None, state = None):
         """ Get hash key of actions in a given state, also returns the hash key of that state """
-
         if state is None:
             state = self.current_state
 
@@ -117,25 +69,6 @@ class Agent:
 
         return next_state_hash, next_actions_hash
 
-    def rewardFunction(self, state, action):
-        """ Returns positive value actions turns into win, else zero """
-
-        # Perform action
-        self.performAction(action, state=state)
-
-        # Check winner
-        winner = state.checkWinner()
-        missed_blocking_move = state.checkWinPossible(self.symbol)
-        if winner == self.symbol:
-                reward = 1
-        elif missed_blocking_move is True:
-                reward = -1
-        else:
-            reward = 0
-        # Revert action
-        self.revertLastAction(state=state)
-
-        return reward
 
     def assignState(self, state):
         """ Assign a state (Board) to the agent"""
@@ -144,7 +77,6 @@ class Agent:
 
     def getBestAction(self):
         """ Get best move from the Trainer that has the largest expected reward """
-
         self.updatePossibleActions()
 
         # Get hash key for state and actions
@@ -155,21 +87,15 @@ class Agent:
     
     
     def loadTrainer(self, save_path):
-        """
-        Load Q-values from another trainer
-        """
-        
+            
         with open(save_path, "rb") as f:
             dict = cPickle.load(f)
-        # print("Q"+str(dict))
+        # print("Q: "+str(dict))
         return Trainer(self, Q=dict)
 
 class Board:
-    """ Class that represents the game board of Tic Tac Toe """
-
     playerX = 1
     playerO = -1
-
     def __init__(self, rows = 3, cols = 3, win_threshold = 3):
         """
             rows (int)
@@ -212,71 +138,8 @@ class Board:
                 factor = 10*factor
         return state_hash
 
-    def checkWinner(self):
-        """  Get winner, if one exists """
-        """ TODO: Not general case, only works for 3x3 board """
-
-        symbols = np.unique(self.state)
-        symbols = list(symbols[np.nonzero(symbols)])
-
-        for sym in symbols:
-
-            # Check rows
-            row= np.any((np.all(self.state == sym, axis=1)))
-
-            # Check columns
-            col = np.any((np.all(self.state == sym, axis=0)))
-
-            # Check diagonals
-            diag1 = np.array([self.state[0,0], self.state[1,1], self.state[2,2]])
-            diag1 = np.all(diag1 == sym)
-            
-            diag2 = np.array([self.state[0,2], self.state[1,1], self.state[2,0]])
-            diag2 = np.all(diag2 == sym)
-
-            # Check if state has winner and return winner in that case
-            if row or col or diag1 or diag2:
-                return sym
-            
-        # No winner found
-        return 0 
-
-    def checkWinPossible(self, last_player_value):
-        """ 
-            Test whether there is a winning move available for the next player.
-            Return True if it is available.
-            last_player (int)
-        """
-
-        # Next player is the negative of last_player_value
-        next_player_player = - last_player_value
-
-        winning_move_found = False
-        for action in self.getAvailablePos():
-            x = action[0]
-            y = action[1]
-
-            # Perform action
-            self.setPosition(x, y, next_player_player)
-
-            # Check if winning move
-            if self.checkWinner() != 0:
-                winning_move_found = True
-
-            # Revert action
-            self.setPosition(x, y, 0)
-
-            # If found, then return True
-            if winning_move_found is True:
-                return True
-
-        return False
-
-    def __str__(self):
-        return str(self.state)
 
 class Trainer:
-
     def __init__(self, agent, learning_parameter = 0.1, discount_factor = 0.9, Q = {}):
         """
             agent (Agent)
@@ -284,7 +147,6 @@ class Trainer:
             discount_factor (float)
             Q (dict)
         """
-
         self.agent = agent
         self.learning_parameter = learning_parameter
         self.discount_factor = discount_factor
@@ -298,7 +160,6 @@ class Trainer:
         """ Get expected reward given an action in a given state,
             returns 0 if the state-action pair has not been seen before.
             Input is state and action hash key                          """
-
         state_action_key = Trainer.getStatePairKey(state_hash, action_hash)
         if state_action_key in self.Q:
             return self.Q.get(state_action_key)
@@ -327,8 +188,6 @@ class Trainer:
         # Pick a random action at first
         random_idx = np.random.choice(list_actions.shape[0])
         best_action = list_actions[random_idx]
-        
-        #print("best_action:"+str(best_action))
 
         # Find action that given largest Q in given state
         maxQ = 0
@@ -338,29 +197,8 @@ class Trainer:
                 maxQ = tmpQ
                 best_action = action
 
-        #print("maxQ:"+str(maxQ))
         return best_action
-        
 
-    def updateQ(self, state, action):
-        """ Implements Q-learning iterative algorithm """
-
-        state_hash = state.getStateHash()
-        action_hash = self.agent.getActionHash(action)
-
-        # Get current Q Value
-        currentQ = self.getValueQ(state_hash, action_hash)
-
-        # Find max Q value given the possible set of actions in the next state
-        next_state, next_actions = self.agent.getActionHashFromState(action=action, state=state)
-        max_nextQ = self.getMaxQ(next_state, next_actions) 
-        
-        # Update new Q
-        newQ = currentQ + self.learning_parameter * (self.agent.rewardFunction(state, action) + self.discount_factor * max_nextQ - currentQ)
-
-        self.setValueQ(state_hash, action_hash, newQ)
-
-## testing
 class Play():
     def __init__(self):
         trained_agent = "hw1_3_data.pkl"
@@ -413,7 +251,6 @@ class Play():
 
     def agentMove(self):
         move = self.agent.getBestAction()
-        time.sleep(random.random()*1 + 0.5)
         self.playMove(move[0], move[1])
 
 
